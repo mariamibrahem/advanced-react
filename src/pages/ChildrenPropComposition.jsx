4194bf975ec26523e269f706e27ab8c8c04df589
mariamibrahem <75806311+mariamibrahem@users.noreply.github.com>
2025-11-24 21:50:25 +0200

    Add ChildrenPropComposition component to demonstrate props and rerenders

---
+++ src/pages/ChildrenPropComposition.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'
+import RerenderMyth from '../tutorials/propsMyth/RerenderMyth'
+import '../App.css'
+
+function ChildrenPropComposition() {
+  return (
+    <div className="tutorial-page">
+      <h1>Children Prop Composition</h1>
+      <p className="tutorial-intro">
+        This tutorial demonstrates that <strong>props are not the cause of rerenders</strong>.
+        Even when a ref value changes, components don't automatically rerender. 
+        This is an important concept to understand when optimizing React performance.
+      </p>
+      
+      <RerenderMyth />
+      
+      <Link to="/" className="back-button">
+        ← Back to Home
+      </Link>
+    </div>
+  )
+}
+
+export default ChildrenPropComposition
+
+
+
